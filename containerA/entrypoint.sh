66d312f98ffc6d00523f2e72babce617178e805f
Alexandre Bezroutchko <abb@gremwell.com>
2024-05-16 22:03:41 +0200

    cleanup

--- containerA/entrypoint.sh
+++ containerA/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# confiure networking
+# configure networking
 ip addr flush dev eth1
 ip route flush dev eth1
 ip addr add 10.0.0.1/24 dev eth1
 
-# Keep the container running
+# keep the container running
 exec sleep inf
--- containerB/entrypoint.sh
+++ containerB/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Flush routes and addresses on eth1 and eth2
+# flush routes and addresses on eth1 and eth2
 ip addr flush dev eth1
 ip route flush dev eth1
 ip addr flush dev eth2
 ip route flush dev eth2
 
-# Create a bridge and add eth1 and eth2 to it
+# create a bridge and add eth1 and eth2 to it
 ip link add name br0 type bridge
 ip link set eth1 master br0 up 
 ip link set eth2 master br0 up
 ip link set br0 up
 
-# Keep the container running
+# keep the container running
 exec sleep inf
